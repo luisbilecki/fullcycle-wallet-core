6da26adc52b8e48ec3017044fb023848500e1db6
Luís Felipe Bilecki <luis.bilecki@gmail.com>
2023-05-01 15:21:05 -0300

    feat: add test for create client usecase

--- go.mod
+++ go.mod
@@ -11,6 +11,8 @@ require (
 	github.com/go-playground/locales v0.14.1 // indirect
 	github.com/go-playground/universal-translator v0.18.1 // indirect
 	github.com/leodido/go-urn v1.2.4 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
+	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 )
 
 require (
---
+++ internal/usecase/client/create_client_test.go
@@ -0,0 +1,28 @@
+package client
+
+import (
+	"testing"
+
+	mockClient "github.com/luisbilecki/fullcycle-wallet-core/internal/gateway/mock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestCreateClientUseCase_Execute(t *testing.T) {
+	clientGateway := &mockClient.ClientGatewayMock{}
+	clientGateway.On("Save", mock.Anything).Return(nil)
+
+	createClientUseCase := NewCreateClientUseCase(clientGateway)
+	input := CreateClientInputDTO{
+		Name:  "John Doe",
+		Email: "c@c.com",
+	}
+	output, err := createClientUseCase.Execute(input)
+
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	assert.Equal(t, input.Name, output.Name)
+	assert.Equal(t, input.Email, output.Email)
+	clientGateway.AssertExpectations(t)
+	clientGateway.AssertNumberOfCalls(t, "Save", 1)
+}
